b953ef39af075b7da1c21698695ccb6f5fff64df
ldas11 <luisdavidanchia@gmail.com>
2021-08-02 15:54:08 +0200

    Delete segway directory

--- Segway/Release/flash.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\Atmel\Flip 3.4.7\bin\batchisp.exe" -device at32uc3b0256 -hardware usb -operation erase f memory flash blankcheck loadbuffer Segway.elf program verify start reset 0
-pause
